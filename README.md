f10d5dc6bdf212369ba9819be526b300ed9c2edc
sahotech <sahotech06gmail.com>
2023-06-20 15:28:51 -0700

    team task

---
+++ README.md
@@ -0,0 +1,10 @@
+HBNB is a complete web application, integrating database storage, HTML/CSS templating, API, front-end and others.
+
+This team project is part of the ALX School Software Engineering program.
+It represents the first step towards building a full web application: the AirBnB clone.
+
+
+##AUTHOR 
+
+## Peter Saho
+## Kesta Nyavor
---
+++ tests/test_models/test_city.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""The Unittest module for the City Class."""
+
+import unittest
+from datetime import datetime
+import time
+from models.city import City
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestCity(unittest.TestCase):
+
+    """The Test Cases for the City class."""
+
+    def setUp(self):
+        """This Sets up test methods."""
+        pass
+
+    def tearDown(self):
+        """This Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """This Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_8_instantiation(self):
+        """This Tests instantiation of City class."""
+
+        b = City()
+        self.assertEqual(str(type(b)), "<class 'models.city.City'>")
+        self.assertIsInstance(b, City)
+        self.assertTrue(issubclass(type(b), BaseModel))
+
+    def test_8_attributes(self):
+        """This Tests the attributes of City class."""
+        attributes = storage.attributes()["City"]
+        o = City()
+        for k, v in attributes.items():
+            self.assertTrue(hasattr(o, k))
+            self.assertEqual(type(getattr(o, k, None)), v)
+
+if __name__ == "__main__":
+    unittest.main()
